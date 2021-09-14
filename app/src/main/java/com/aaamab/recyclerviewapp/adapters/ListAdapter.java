637508675c83bffe8efc320fb36e3fd8f918ffdb
Ahmed <aaamab257@gmail.com>
2021-09-14 13:19:04 +0200

    first commit

--- app/src/main/java/com/aaamab/recyclerviewapp/adapters/ListAdapter.java
+++ app/src/main/java/com/aaamab/recyclerviewapp/adapters/ListAdapter.java
@@ -1,33 +1,55 @@
 package com.aaamab.recyclerviewapp.adapters;
 
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.aaamab.recyclerviewapp.R;
+
+import java.util.ArrayList;
+
 public class ListAdapter extends RecyclerView.Adapter<ListAdapter.MyViewHolder> {
+
+    ArrayList<String> listData ;
+    Context context ;
+
+    public ListAdapter(ArrayList<String> listData, Context context) {
+        this.listData = listData;
+        this.context = context;
+    }
 
     @NonNull
     @Override
     public ListAdapter.MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        View view = LayoutInflater.from(context)
+                .inflate(R.layout.list_item, parent, false);
+        return new MyViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull ListAdapter.MyViewHolder holder, int position) {
+        holder.name_txt.setText(listData.get(position));
 
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return listData.size();
     }
+
 
     public class MyViewHolder extends RecyclerView.ViewHolder {
 
+        TextView name_txt ;
+
         public MyViewHolder(@NonNull View itemView) {
             super(itemView);
+            name_txt = itemView.findViewById(R.id.txt_name);
         }
     }
 }
